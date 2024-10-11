a8b2f30bc54878b11d17bb84c65b04d5cce8e502
Udoozy <agbo4samson@gmail.com>
2024-10-11 15:45:43 +0000

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function (boolean) {
+ return new Promise((resolve, reject) => {
+   const myObj = {
+     status: 200,
+     body: 'Success'
+   };
+   if (boolean === true) {
+     resolve(myObj);
+   } else {
+     reject(Error('The fake API is not working currently'));
+   }
+ });
+}
