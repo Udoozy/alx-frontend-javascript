2fd30c4056810f70c6b5cedb11d87eeb5d44b9de
Udoozy <123296516+Udoozy@users.noreply.github.com>
2024-11-27 22:00:07 +0000

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+export default function updateStudentGradeByCity(anArray, city, newGrades) {
+  return anArray
+    .filter(student => student.location === city)
+    .map(student => {
+      let grade = newGrades.find(g => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+    });
+}
